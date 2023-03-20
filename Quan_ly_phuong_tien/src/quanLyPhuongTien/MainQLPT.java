d2a150489eeffa0b5f66a2f4ac9a1676a43b0d6f
sinhtran22 <tran.viet.sinh@sun-asterisk.com>
2023-03-20 13:32:14 +0700

    fix validate

--- Quan_ly_phuong_tien/src/quanLyPhuongTien/MainQLPT.java
+++ Quan_ly_phuong_tien/src/quanLyPhuongTien/MainQLPT.java
@@ -10,17 +10,17 @@ public class MainQLPT {
 	public static void main(String[] args) {
 		VehicleManagement ql = new VehicleManagement();
 
-		ql.addVehicle(new Car("29-C1", "Yamaha", 2002, "Yellow", new Owner("12345", "Truong Tuan Tu", "akhe@gmail.com"), 4, "gasoline"));
-		ql.addVehicle(new Car("43-C1", "Toyota", 1998, "Red", new Owner("23456", "Phung Thanh Do", "ekha@gmail.com"), 6, "diesel"));
-		ql.addVehicle(new Car("29-C1", "Honda", 2000, "Green", new Owner("34567", "Ho Quang Hieu", "kdek@gmail.com"), 12, "diesel"));
-		ql.addVehicle(new Motobike("56-B2", "Yamaha", 2005, "Black", new Owner("45678", "Nguyen Tan Dat", "ank@gmail.com"), 160));
-		ql.addVehicle(new Motobike("25-Y1", "Honda", 1999, "Pink", new Owner("56789", "Bao Thanh Thien", "ctn@gmail.com"), 125));
-		ql.addVehicle(new Motobike("92-F1", "Yamaha", 1993, "Green", new Owner("67891", "Tran Viet Sinh", "tkdemm@gmail.com"), 150));
-		ql.addVehicle(new Truck("67-H1", "Suzuki", 2000, "Blue", new Owner("78910", "Nguyen Hoang Long", "btck@gmail.com"), 2000));
-		ql.addVehicle(new Truck("26-C1", "Suzuki", 1998, "White", new Owner("34567", "Nguyen Lam Khue", "nmte@gmail.com"), 5000));
-		ql.addVehicle(new Truck("92-G1", "Toyota", 2001, "Gray", new Owner("91011", "Le Cong Huy", "mtnm@gmail.com"), 10000));
+		ql.addVehicle(new Car("29534", "Yamaha", 2015, "Yellow", new Owner("123456789123", "Truong Tuan Tu", "akhe@gmail.com"), 4, "gasoline"));
+		ql.addVehicle(new Car("43222", "Toyota", 2017, "Red", new Owner("234567234567", "Phung Thanh Do", "ekha@gmail.com"), 6, "diesel"));
+		ql.addVehicle(new Car("29657", "Honda", 2023, "Green", new Owner("345678345678", "Ho Quang Hieu", "kdek@gmail.com"), 12, "diesel"));
+		ql.addVehicle(new Motobike("56123", "Yamaha", 2021, "Black", new Owner("456789456789", "Nguyen Tan Dat", "ank@gmail.com"), 160));
+		ql.addVehicle(new Motobike("25498", "Honda", 2022, "Pink", new Owner("567891567891", "Bao Thanh Thien", "ctn@gmail.com"), 125));
+		ql.addVehicle(new Motobike("92524", "Yamaha", 2018, "Green", new Owner("678910678910", "Tran Viet Sinh", "tkdemm@gmail.com"), 150));
+		ql.addVehicle(new Truck("67007", "Suzuki", 2019, "Blue", new Owner("789101789101", "Nguyen Hoang Long", "btck@gmail.com"), 2000));
+		ql.addVehicle(new Truck("26309", "Suzuki", 2021, "White", new Owner("345679345679", "Nguyen Lam Khue", "nmte@gmail.com"), 5000));
+		ql.addVehicle(new Truck("92126", "Toyota", 2020, "Gray", new Owner("910111910111", "Le Cong Huy", "mtnm@gmail.com"), 10000));
 		
-	    Vehicle v1 = ql.findByLicenseNumber("92-F1");
+	    Vehicle v1 = ql.findByLicenseNumber("92524");
 	    System.out.println("2 - Phuong tien tim theo so xe: ");
 	    System.out.println("- Số xe: " + v1.getLicenseNumber());
 	    System.out.println("- Nhà sản xuất: " + v1.getManufacturer());
@@ -28,7 +28,7 @@ public class MainQLPT {
 	    System.out.println("- Màu xe: " + v1.getColor());
 	    
 	    System.out.println("---------------------------");
-	    List<Vehicle> v2 = ql.findByID("34567");
+	    List<Vehicle> v2 = ql.findByID("345679345679");
 	    System.out.println("3 - Tim PTGT cua chu xe co so CMND tuong ung: " + v2.size());
 	    
 	    System.out.println("----------------------------------------");
